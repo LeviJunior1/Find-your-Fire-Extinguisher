91c803fdde46496fe68eb39e4b91e25cc4168e95
LeviJunior12 <levicoc123@gmail.com>
2021-11-15 10:03:08 -0300

    first commit

---
+++ layouts/no-nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <NuxtLink to="/">Go Back</NuxtLink>
+        <Nuxt />
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        padding-top: 2rem;
+    }
+</style>
+
